6e250d0bc637f738467583c851595e1ab2a7f999
woong <103757515+woong12@users.noreply.github.com>
2024-06-18 18:53:56 +0900

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python3
+
+# This is the same as mjpeg_server.py, but uses the h/w MJPEG encoder.
+
+import io
+import logging
+import socketserver
+from http import server
+from threading import Condition
+from datetime import datetime
+
+from picamera2 import Picamera2
+from picamera2.encoders import MJPEGEncoder
+from picamera2.outputs import FileOutput
+
+PAGE = """\
+<html>
+<head>
+<meta name="viewport" content="width=device-width. initial-scale=1.0"/>
+<title>picamera2 MJPEG streaming demo</title>
+<style>
+    * {
+        box-sizing: border-box;
+        margin: 0;
+    }
+        
+    body {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: top;
+        align-items: center;
+        flex-direction: column;
+        position: absolute;
+        object-fit: cover;
+        background-color: #ffffff;
+    }
+    
+    .box {
+        width: 400px;
+        height: 280px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 25px;
+        position: relative;
+        margin-top: 10px;
+        overflow: hidden;
+        box-shadow: rgba(0, 0, 0, 0.5) 0px 10px 20px;
+    }
+    
+    
+</style>
+</head>
+<body>
+
+<div class="box">
+    <img src="stream.mjpg" width="400" height="280" />
+</div>
+<!--
+<form action="/capture">
+    <button type="submit">Capture</button>
+    -->
+</body>
+</html>
+"""
+
+
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
+
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
+
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        # elif self.path == '/capture':
+            # self.send_response(200)
+            # self.send_header('Content-type', 'text/plain')
+            # self.end_headers()
+            # try:
+                # capture_frsame()
+                # self.wfile.write(b'Capture successful')
+            # except Exception as e:
+                # logging.warning('Error capturing frame: %s', str(e))
+                # self.wfile.write(b'Error capturing frame')
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+    
+    
+# def capture_frame():
+    # if output.frame is None:
+        # raise Exception('No frame aval')
+    # capture_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    # with open(f'capture_{capture_time}.jpg', 'wb') as file:
+        # file.write(output.frame)
+
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_video_configuration(main={"size": (400, 280)}))
+# picam2.framerate = 30
+output = StreamingOutput()
+picam2.start_recording(MJPEGEncoder(), FileOutput(output))
+
+try:
+    address = ('', 8000)
+    server = StreamingServer(address, StreamingHandler)
+    server.serve_forever()
+finally:
+    picam2.stop_recording()
---
+++ control.py
@@ -0,0 +1,33 @@
+
+#!/usr/bin/env python3
+
+from flask import Flask, render_template
+import smbus
+import time
+
+app = Flask(__name__, template_folder='views')
+ 
+# Raspberry Pi Zero W's I2C bus number
+bus = smbus.SMBus(1)
+
+# Arduino Mega's I2C address
+ARDUINO_ADDRESS = 0x08
+
+# Function to send command to Arduino
+def send_data(data):
+    bus.write_i2c_block_data(ARDUINO_ADDRESS, 0, [ord(c) for c in data])
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Define route to handle commands
+@app.route('/send_command/<command>')
+def handle_command(command):
+    send_data(command)  # Corrected function name
+    return f"Command '{command}' sent to Arduino."
+
+if __name__ == '__main__':
+    #app.run(debug=True, host='127.0.1.1', port=5001)
+    app.run(host='10.251.145.132')
+    app.run(host='0.0.0.0')
